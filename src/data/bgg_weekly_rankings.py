280a455fd8be3bf4731403671981117b51a6ff01
MichoelSnow <protect3f@gmail.com>
2018-10-15 20:01:31 -0400

    updated requirements

--- src/data/bgg_weekly_rankings.py
+++ src/data/bgg_weekly_rankings.py
@@ -1,10 +1,11 @@
 from urllib import request
+import urllib3
 from bs4 import BeautifulSoup, Comment
 import pandas as pd
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from datetime import datetime
 import json
-
-
 
 
 def extract_gm_id(soup):
@@ -14,20 +15,24 @@ def extract_gm_id(soup):
         id_list.append(int(row.find_all('a')[1]['href'].split('/')[2]))
     return id_list
 
+
 def top_2k_gms(pg_gm_rnks):
     gm_ids = []
-    for pg_num in range(1,21):
+    for pg_num in range(1, 21):
         pg = request.urlopen(f'{pg_gm_rnks}{str(pg_num)}')
         soup = BeautifulSoup(pg, 'html.parser')
         gm_ids += extract_gm_id(soup)
     return gm_ids
 
+
 def extract_game_item(item):
     gm_dict = {}
     field_int = ['yearpublished', 'minplayers', 'maxplayers', 'playingtime', 'minplaytime', 'maxplaytime', 'minage']
-    field_categ = ['boardgamecategory', 'boardgamemechanic', 'boardgamefamily','boardgamedesigner', 'boardgameartist', 'boardgamepublisher']
+    field_categ = ['boardgamecategory', 'boardgamemechanic', 'boardgamefamily', 'boardgamedesigner', 'boardgameartist',
+                   'boardgamepublisher']
     field_rank = [x['friendlyname'] for x in item.find_all('rank')]
-    field_stats = ['usersrated', 'average', 'bayesaverage', 'stddev', 'median', 'owned', 'trading', 'wanting', 'wishing', 'numcomments', 'numweights', 'averageweight']
+    field_stats = ['usersrated', 'average', 'bayesaverage', 'stddev', 'median', 'owned', 'trading', 'wanting',
+                   'wishing', 'numcomments', 'numweights', 'averageweight']
     gm_dict['name'] = item.find('name')['value']
     gm_dict['id'] = item['id']
     gm_dict['num_of_rankings'] = int(item.find('comments')['totalitems'])
@@ -38,13 +43,13 @@ def extract_game_item(item):
         else:
             gm_dict[i] = int(field_val['value'])
     for i in field_categ:
-        gm_dict[i] = [x['value'] for x in item.find_all('link',{'type':i})]
+        gm_dict[i] = [x['value'] for x in item.find_all('link', {'type': i})]
     for i in field_rank:
-        field_val = item.find('rank',{'friendlyname':i})
+        field_val = item.find('rank', {'friendlyname': i})
         if field_val is None or field_val['value'] == 'Not Ranked':
-            gm_dict[i.replace(' ','')] = -1
+            gm_dict[i.replace(' ', '')] = -1
         else:
-            gm_dict[i.replace(' ','')] = int(field_val['value'])
+            gm_dict[i.replace(' ', '')] = int(field_val['value'])
     for i in field_stats:
         field_val = item.find(i)
         if field_val is None:
@@ -53,17 +58,31 @@ def extract_game_item(item):
             gm_dict[i] = float(field_val['value'])
     return gm_dict
 
+
 def create_df_gm_ranks(gm_ids, bs_pg_gm):
     gm_list = []
     idx_split = 4
-    idx_size = int(len(gm_ids)/idx_split)
+    idx_size = int(len(gm_ids) / idx_split)
+    http = urllib3.PoolManager()
     for i in range(idx_split):
-        idx = str(gm_ids[i*idx_size:(i+1)*idx_size]).replace(' ','')[1:-1]   
-        pg = request.urlopen(f'{bs_pg_gm}{str(idx)}')
-        xsoup = BeautifulSoup(pg, 'xml')
+        print(i, str(datetime.now().time())[:8])
+        idx = str(gm_ids[i * idx_size:(i + 1) * idx_size]).replace(' ', '')[1:-1]
+        pg = http.request('GET', f'{bs_pg_gm}{str(idx)}', retries=10)
+        xsoup = BeautifulSoup(pg.data, 'xml')
         gm_list += [extract_game_item(x) for x in xsoup.find_all('item')]
     df = pd.DataFrame(gm_list)
     return df
+
+
+def update_json(gm_ids):
+    with open('../data/kaggle/dataset-metadata.json', 'rb') as f:
+        meta_dict = json.load(f)
+    meta_dict['resources'].append({
+        'path': f'bgg_top{len(gm_ids)}_{str(datetime.now().date())}.csv',
+        'description': f'Board Game Geek top 2000 games on {str(datetime.now().date())}'
+    })
+    with open('../data/kaggle/dataset-metadata.json', 'w') as fp:
+        json.dump(meta_dict, fp)
 
 
 pg_gm_rnks = 'https://boardgamegeek.com/browse/boardgame/page/'
@@ -71,6 +90,6 @@ gm_ids = top_2k_gms(pg_gm_rnks)
 bs_pg = 'https://www.boardgamegeek.com/xmlapi2/'
 bs_pg_gm = f'{bs_pg}thing?type=boardgame&stats=1&ratingcomments=1&page=1&pagesize=10&id='
 df = create_df_gm_ranks(gm_ids, bs_pg_gm)
-df.to_csv(f'../../data/kaggle/bgg_top{len(gm_ids)}_{str(datetime.now().date())}.csv', index=False)
-
-
+#print(df.head())
+df.to_csv(f'../../data/kaggle/bgg_top{len(gm_ids)}_{str(datetime.now().date())}_tmp.csv', index=False)
+update_json(gm_ids)
